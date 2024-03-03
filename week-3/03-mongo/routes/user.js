b40e3c1523f375b3df5cc0941f18509d85028db9
Rajdeep4050 <rajdeepchaurasia14331433@gmail.com>
2024-03-04 01:14:31 +0530

    week3 ka 03 and 04 partial

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,65 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const { User, Course } = require("../db");
+const { default: mongoose } = require("mongoose");
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  User.create({
+    username,
+    password,
+  });
+  res.json({
+    message: "User created",
+  });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const response = await Course.find({});
+  res.json({
+    courses: response,
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  const courseId = req.params.courseId;
+  const username = req.headers.username;
+  await User.updateOne(
+    {
+      username: username,
+    },
+    {
+      $push: {
+        purchasedCourse: new mongoose.Types.ObjectId(courseId),
+      },
+    }
+  );
+  res.json({
+    message: "Purchase completed",
+  });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  const user = await User.findOne({
+    username: req.headers.username,
+  });
+
+  console.log(user.purchasedCourse);
+  const courses = await Course.find({
+    _id: {
+      $in: user.purchasedCourse,
+    },
+  });
+
+  res.json({
+    courses: courses,
+  });
 });
 
-module.exports = router
+module.exports = router;
